4ee7009dd2ce327b26edd7cae2b4a53630ce005c
Meaxis <37759268+Meaxis@users.noreply.github.com>
2020-04-16 20:52:33 +0200

    Made code more readable

--- ballcounter.php
+++ ballcounter.php
@@ -1,25 +1,24 @@
 <?php
-  /*
-    We stack tennis balls to make a square-based py ramide.
+	/*
+		We stack tennis balls to make a square-based py ramide.
 
-    At the very top, on the first floor, there's a ball.
-    On the second floor, there are 2 to the power of 2, or 4 balls, or 5 balls in all.
-    On the third floor, there are 3 to the power of 2, or 9 balls, or 14 balls in all.
-    In the 4th, there are 4 to the power of 2, that is to say 16 balls, or 30 balls in all.
+		At the very top, on the first floor, there's a ball.
+		On the second floor, there are 2 to the power of 2, or 4 balls, or 5 balls in all.
+		On the third floor, there are 3 to the power of 2, or 9 balls, or 14 balls in all.
+		In the 4th, there are 4 to the power of 2, that is to say 16 balls, or 30 balls in all.
+		etc...
+	*/
 
-    How much balls would you have with 50 floors? 
-  */
-  
-  // Begin with one ball
+	// Begin with 1
 	$total = 1;
-  
-  // Iterate 50 times
-	for ($i=0; $i < 50; $i++) 
+
+	// Iterate 50 times
+	for ($i=1; $i < 50; $i++) 
 	{
-    // Do the maths
+		// Do the maths
 		$total = $total+($i^2);
 	}
-  
-  // Return the complete number
+
+	// Return total
 	echo $total;
 ?>
